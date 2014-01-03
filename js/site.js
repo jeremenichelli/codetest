46200a1bb9d7bac3aaa077cb1d2409b9ceb62b98
Jere Menichelli <j.menichelli@globant.com>
2014-01-03 13:17:47 -0300

    Sorting, pagination, hash functionality

--- js/site.js
+++ js/site.js
@@ -1,44 +1,118 @@
-var ETSY = ETSY || {};
-var results;
+function ResultsViewModel() {
+    var self = this;
+    self.searchResults = ko.observableArray([]);
 
-ETSY.search = (function(){
-	var resultsArea = document.querySelector('.results ul');
-	var _init = function(){
-		$('.search-btn').on('click', function(e){
-			e.preventDefault();
-			var kw = $('.search-field').val();
-			_load(kw);
-		})
-	};
-	var _load = function(keywords){
-		$('.loader').fadeIn(200);
-		api_key = "nj6utllday5j59spvegtu2np";
-	    etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords="+keywords+"&limit=10&includes=Images:1&api_key="+api_key;
-	    resultsArea.style.display = "none";
-	    $.ajax({
-	        url: etsyURL,
-	        dataType: 'jsonp',
-	        success: function(data) {
-	        	$('.loader').fadeOut(150);
-	            if (data.ok) {
-	                if (data.count > 0) {
-	                	results = data.results;
-	                    var dataLength = data.results.length;
-	                    var tmpString = "";	
-	                    resultsArea.innerHTML = tmpString;                    
-	                    for (var i = 0; i < dataLength; i++) {
-	                    	tmpString = '<li><div class="row"><div class="one column"><i class="icon-menu"></i></div><div class="three column">'+
-	                    		'<img class="product-img" height="135" width="170" src="'+data.results[i].Images[0].url_170x135+'"></div>';
-	                    	tmpString += '<div class="five column"><h2>'+data.results[i].title+
-	                    		'</h2><p class="price">$'+data.results[i].price+'</p>';
-                    		tmpString += '<a class="product-btn icon-link" href="'+data.results[i].url+'" target="_blank">Go to the product page</a></div>';
-                    		tmpString += '<div class="three column"><a href="#" class="remove-btn icon-cancel">Remove</a>'+
-                    			'<a href="javascript:void(0);" class="add-btn icon-basket">Add to cart</a><a href="#" class="view-more-btn icon-plus">View Description</a></div>';
-	                    	tmpString += '<div class="twelve column details">'+data.results[i].description+'</div></li>';
-	                    	resultsArea.innerHTML += tmpString;
-	                    	$('.results ul').sortable({
-								handle: '.icon-menu'
-							});
+	var ETSY = ETSY || {};
+	var results;
+
+	ETSY.search = (function(){
+		var resultsArea = $('.results');
+		var toolbar = $('.toolbar');
+		var offset = 0;
+		var slug;
+		var _init = function(){
+			$('.search-btn').on('click', function(e){
+				e.preventDefault();
+				offset = 0;
+				var kw = $('.search-field').val();
+				$('.prev').hide();
+				_load("?keywords="+kw+"&limit=10&includes=Images:1&api_key=nj6utllday5j59spvegtu2np");
+			});
+			$('.next').on('click', function(e){
+				e.preventDefault();
+				offset = offset+10;
+				_load(window.location.hash.split('#')[1].split('&offset')[0]+"&offset="+offset);
+				$('.prev').show();
+			});
+			$('.prev').on('click', function(e){
+				e.preventDefault();
+				offset = offset-10;
+				_load(window.location.hash.split('#')[1].split('&offset')[0]+"&offset="+offset);
+				if (offset==0){
+					$(this).hide();
+				}
+			});
+			$('.sort-less-price').on('click', function(e){
+				e.preventDefault();
+				_sortLessPrice();
+			});
+			$('.sort-more-price').on('click', function(e){
+				e.preventDefault();
+				_sortMorePrice();
+			});
+			$('.sort-less-name').on('click', function(e){
+				e.preventDefault();
+				_sortLessName();
+			});
+			$('.sort-more-name').on('click', function(e){
+				e.preventDefault();
+				_sortMoreName();
+			});
+			$(window).on('load', function(){
+				_loadHash();
+			});
+		};
+		var _sortLessPrice = function(){
+			self.searchResults.sort(function(a, b){
+				var aPrice = parseInt(a.price);
+				var bPrice = parseInt(b.price);
+				if (aPrice > bPrice)
+			      return 1;
+			    if (aPrice < bPrice)
+			      return -1;
+			    // a must be equal to b
+			    return 0;
+			});
+		};
+		var _sortMorePrice = function(){
+			self.searchResults.sort(function(a, b){
+				var aPrice = parseInt(a.price);
+				var bPrice = parseInt(b.price);
+				if (aPrice < bPrice)
+			      return 1;
+			    if (aPrice > bPrice)
+			      return -1;
+			    // a must be equal to b
+			    return 0;
+			});
+		};
+		var _sortLessName = function(){
+			self.searchResults.sort(function(a, b){
+				if (a.title > b.title)
+			      return 1;
+			    if (a.title < b.title)
+			      return -1;
+			    // a must be equal to b
+			    return 0;
+			});
+		};
+		var _sortMoreName = function(){
+			self.searchResults.sort(function(a, b){
+				if (a.title < b.title)
+			      return 1;
+			    if (a.title > b.title)
+			      return -1;
+			    // a must be equal to b
+			    return 0;
+			});
+		};
+		var _load = function(slug){
+			$('.loader').fadeIn(200);
+		    etsyURL = "https://openapi.etsy.com/v2/listings/active.js"+slug;
+		    resultsArea.hide();
+		    toolbar.hide();
+		    $.ajax({
+		        url: etsyURL,
+		        dataType: 'jsonp',
+		        success: function(data) {
+		        	$('.loader').fadeOut(150);
+		            if (data.ok) {
+		                if (data.count > 0) {
+		                    var dataLength = data.results.length;
+		                    self.searchResults.removeAll();
+		                	for (var i = 0; i < dataLength; i++) {
+		                		self.searchResults.push(data.results[i]);
+		                	};
 							$('.remove-btn').on('click', function(e){
 								e.preventDefault();
 								_removeRow($(this));
@@ -47,37 +121,46 @@ ETSY.search = (function(){
 								e.preventDefault();
 								_toggleDescription($(this));
 							})
-	                    };
-	                } else {
-	                    tmpString = '<div class="row no-results"><div class="twelve column"><p>No results were found for "'+keywords
-	                    	+'", please try again with different words.</p></div></div>';
-	                	resultsArea.innerHTML = tmpString;
-	                }
-	            } else {
-	                tmpString = '<div class="row no-results"><div class="twelve column"><p>Ups! It seems like something went wrong, try again and if the error persists please contact us. Thanks you!</p></div></div>';
-	                resultsArea.innerHTML = tmpString;
-	            }
-	        $('.results ul').fadeIn(250);
-	        }
-	    });
-	};
-	var _removeRow = function(btn){
-		btn.closest('li').fadeOut(250, function(){
-			$(this).remove();
-		})
-	}
-	var _toggleDescription = function(btn){
-		if(btn.hasClass('icon-plus')){
-			btn.removeClass('icon-plus').addClass('icon-minus').text('Hide description');
-		} else {
-			btn.removeClass('icon-minus').addClass('icon-plus').text('View description');
+	            			toolbar.fadeIn(150);
+	            			window.location.hash = slug;
+		                } else {
+		                    resultsArea.html('<div class="row no-results"><div class="twelve column"><p>No results were found for "'+keywords
+		                    	+'", please try again with different words.</p></div></div>');
+		                }
+		            } else {
+		                resultsArea.html('<div class="row no-results"><div class="twelve column"><p>Ups! It seems like something went wrong, try again and if the error persists please contact us. Thank you!</p></div></div>');
+		            }
+		        resultsArea.fadeIn(250);
+		        }
+		    });
+		};
+		var _removeRow = function(btn){
+			btn.closest('li').fadeOut(250, function(){
+				$(this).remove();
+			})
+		};
+		var _toggleDescription = function(btn){
+			if(btn.hasClass('icon-plus')){
+				btn.removeClass('icon-plus').addClass('icon-minus').text('Hide description');
+			} else {
+				btn.removeClass('icon-minus').addClass('icon-plus').text('View description');
+			}
+			btn.closest('li').find('.details').slideToggle(150);
+		};
+		var _loadHash = function(){
+			var hash = window.location.hash.split('#');
+			if (hash.length > 1)
+				_load(hash[1]);
+		};
+		return {
+			init : _init,
+			load : _load,
 		}
-		btn.closest('li').find('.details').slideToggle(150);
-	}
-	return {
-		init : _init,
-		load : _load
-	}
-}()); 
+	}()); 
 
-ETSY.search.init();
+	ETSY.search.init();
+
+}
+
+ko.applyBindings(new ResultsViewModel());
+
